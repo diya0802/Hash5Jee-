8cd5ea3e1b5a2fc3650321ee85c7ac2c1395811e
Diya <diyabisht1963@gmail.com>
2023-10-16 12:22:09 +0530

    Task 2

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function placed() {
+  document.getElementById("success-message").style.display = "block";
+}
+
+(function () {
+  "use strict";
+
+  var forms = document.querySelectorAll(".needs-validation");
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
